fb1650b4089f557fb1307b1ab4cb1c01c370d2b6
barnabas <xobarneysolomon@gmail.com>
2022-07-28 10:38:00 +0300

    finished defining the tables

--- car_rental_application_script.sql
+++ car_rental_application_script.sql
@@ -1,7 +1,7 @@
 create database car_rental_database;
 use car_rental_database;
 
-go
+GO
 
 create table profile_type(
     prof_id varchar(50) primary key,
@@ -22,11 +22,13 @@ create table profile(
 create table customer(
     login_id varchar(200),
     reputation varchar(50) not null, 
-    constraint fk_c_id foreign key(login_id) REFERENCES profile(login_id))
+    constraint fk_c_id foreign key(login_id) REFERENCES profile(login_id)
+    )
 
 create table admin(
     login_id varchar(200), 
     salary money
+    constraint fk_ad_id foreign key(login_id) REFERENCES profile(login_id)
 )
 
 create table branch(
@@ -35,7 +37,7 @@ create table branch(
     branch_rating decimal(2,1)
 )
 
- create table car(
+ create table cars(
      license_plate_no varchar(200) primary key, 
      car_name varchar(100) not null, 
      car_type varchar(100) not null,
@@ -43,20 +45,40 @@ create table branch(
      car_model varchar(100) not null,
      car_color varchar(50) not null,
      car_status TINYINT not null,
-     car_rating int default 'unrated',
+     car_rating int default 0,
      price_per_hour money not null,
      car_branch nvarchar(100), 
-     renter_login_id varchar(200),
+     renter_login_id varchar(200) default 'company'''+'s',
      CONSTRAINT fk_renter_id FOREIGN KEY(renter_login_id) REFERENCES profile(login_id),
      CONSTRAINT fk_car_branch FOREIGN KEY(car_branch) REFERENCES branch(branch_address)
   )
 
-create table rental(
-
-
-    
+create table payment(
+    payment_id int PRIMARY KEY, 
+    payment_type varchar(50) not null
 )
 
-declare @rating decimal(2,1);
-set @rating = 2.923
-print @rating
+create table rental(
+    rent_id VARCHAR(200) PRIMARY KEY,
+    c_login_id varchar(200),
+    rental_date date not null,
+    total_vehicles int not null,  /*new column*/
+    return_date date not null,
+    paid_amount money,
+    payment_id int,
+    CONSTRAINT fk_cid FOREIGN KEY(c_login_id) REFERENCES profile(login_id),
+    CONSTRAINT fk_pmnt_id FOREIGN KEY(payment_id) REFERENCES payment(payment_id)
+)
+
+
+create table rented_cars(
+    r_id VARCHAR(200) PRIMARY KEY,
+    license_plate_no varchar(200)
+    CONSTRAINT fk_r_id FOREIGN KEY(r_id) REFERENCES rental(rent_id),
+    CONSTRAINT fk_lp_num FOREIGN KEY(license_plate_no) REFERENCES cars(license_plate_no)
+)
+
+
+GO
+
+
