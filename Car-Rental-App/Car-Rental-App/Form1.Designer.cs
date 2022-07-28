6f586824d8c40880b56a2931367757212829b4e2
barnabas <xobarneysolomon@gmail.com>
2022-07-28 17:54:56 +0300

    started with the starting page

--- Car-Rental-App/Car-Rental-App/Form1.Designer.cs
+++ Car-Rental-App/Car-Rental-App/Form1.Designer.cs
@@ -29,13 +29,50 @@ namespace Car_Rental_App
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.welcomelbl = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Car_Rental_App.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(437, 438);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // welcomelbl
+            // 
+            this.welcomelbl.AutoSize = true;
+            this.welcomelbl.Font = new System.Drawing.Font("Arial", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.welcomelbl.Location = new System.Drawing.Point(461, 25);
+            this.welcomelbl.Name = "welcomelbl";
+            this.welcomelbl.Size = new System.Drawing.Size(316, 34);
+            this.welcomelbl.TabIndex = 1;
+            this.welcomelbl.Text = "Welcome to Rent a Car";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 438);
+            this.Controls.Add(this.welcomelbl);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label welcomelbl;
     }
 }
 
