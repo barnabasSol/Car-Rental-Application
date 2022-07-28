99a7849619ef0cfa486e9d09380054da40927806
barnabas <xobarneysolomon@gmail.com>
2022-07-28 20:04:17 +0300

    added an icon and and image

--- Car-Rental-App/Car-Rental-App/Form1.Designer.cs
+++ Car-Rental-App/Car-Rental-App/Form1.Designer.cs
@@ -29,10 +29,32 @@ namespace Car_Rental_App
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.welcomelbl = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // welcomelbl
+            // 
+            this.welcomelbl.AutoSize = true;
+            this.welcomelbl.Font = new System.Drawing.Font("Arial", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.welcomelbl.Location = new System.Drawing.Point(475, 20);
+            this.welcomelbl.Name = "welcomelbl";
+            this.welcomelbl.Size = new System.Drawing.Size(316, 34);
+            this.welcomelbl.TabIndex = 1;
+            this.welcomelbl.Text = "Welcome to Rent a Car";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Car_Rental_App.Properties.Resources.icon;
+            this.pictureBox2.Location = new System.Drawing.Point(752, 359);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(67, 67);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
             // 
             // pictureBox1
             // 
@@ -44,25 +66,19 @@ namespace Car_Rental_App
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // welcomelbl
-            // 
-            this.welcomelbl.AutoSize = true;
-            this.welcomelbl.Font = new System.Drawing.Font("Arial", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.welcomelbl.Location = new System.Drawing.Point(461, 25);
-            this.welcomelbl.Name = "welcomelbl";
-            this.welcomelbl.Size = new System.Drawing.Size(316, 34);
-            this.welcomelbl.TabIndex = 1;
-            this.welcomelbl.Text = "Welcome to Rent a Car";
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 438);
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(831, 438);
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.welcomelbl);
             this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Rent a Car";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -73,6 +89,7 @@ namespace Car_Rental_App
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label welcomelbl;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
